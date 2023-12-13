b3ac10776f0928e2a9dea1d289300efdff980418
narkhedepratik <pratiknarkhede039@gmail.com>
2023-12-13 18:52:31 +0530

    app structure

---
+++ UserApp2/src/main/java/com/cjc/main/UserApp2Application.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApp2Application.class, args);
+	}
+
+}
---
+++ UserApp2/src/main/java/com/cjc/main/model/User.java
@@ -0,0 +1,20 @@
+package com.cjc.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	private int uid;
+	private String uname;
+	private String age;
+	private String password;
+	
+	
+	
+	
+}
