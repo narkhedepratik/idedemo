4925145782948eb75061214abd2f74533c041f5f
hp <tejasdhankavade08@gmail.com>
2023-12-13 19:12:35 +0530

    post api

--- UserApp2/src/main/java/com/cjc/main/controller/UserController.java
+++ UserApp2/src/main/java/com/cjc/main/controller/UserController.java
@@ -1,8 +1,21 @@
 package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.model.User;
 
 @RestController
 public class UserController {
+	
+	@PostMapping("/user")
+	public void saveUser(@RequestBody User user)
+	{
+		System.out.println(user.getUid());
+		System.out.println(user.getUname());
+		System.out.println(user.getAge());
+		System.out.println(user.getPassword());
+	}
 
 }
