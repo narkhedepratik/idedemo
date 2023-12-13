feb4805371112d3a2a705eb7d4f79698389b3e4d
ss <ss@ASUS>
2023-12-13 19:23:48 +0530

    get api

--- UserApp2/src/main/java/com/cjc/main/controller/UserController.java
+++ UserApp2/src/main/java/com/cjc/main/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.cjc.main.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.model.User;
 
 @RestController
 public class UserController {
+	
+	
+	@GetMapping("/user")
+	public List<User> getalldata()
+	{
+		List<User> user=new ArrayList<>();
+		
+		return user;
+	}
 
 }
