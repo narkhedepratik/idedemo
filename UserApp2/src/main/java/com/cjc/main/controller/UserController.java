2cc55ba9143a1f68d5f9d236275981192b25a5ef
narkhedepratik <pratiknarkhede039@gmail.com>
2023-12-13 19:48:07 +0530

    user apis

--- UserApp2/src/main/java/com/cjc/main/controller/UserController.java
+++ UserApp2/src/main/java/com/cjc/main/controller/UserController.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjc.main.model.User;
 
 @RestController
 public class UserController {
+	
+	@DeleteMapping("/deleteUser")
+	public String deleteUser()
+	{
+		return "No User";
+	}
 
 	@PostMapping("/user")
 	public void saveUser(@RequestBody User user)
